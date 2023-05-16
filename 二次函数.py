bc2af1d583331c9c91e9007fd30449c350420bdc
NormalTable5801 <61955835+NormalTable5801@users.noreply.github.com>
2023-05-16 21:48:18 +0800

    Add files via upload

---
+++ 二次函数.py
@@ -0,0 +1,355 @@
+import turtle as t;
+while True:
+    t.speed(0)
+    t.pencolor("blue")
+    t.pendown()
+    t.goto(1000,0)
+    t.goto(-1000,0)
+    t.goto(0,0)
+    t.goto(0,1000)
+    t.goto(0,-1000)
+    t.penup()
+
+    t.pencolor("green")
+    t.goto(0,50)
+    t.pendown()
+    t.goto(1000,50)
+    t.goto(-1000,50)
+    t.penup()
+
+    t.goto(0,100)
+    t.pendown()
+    t.goto(1000,100)
+    t.goto(-1000,100)
+    t.penup()
+
+    t.goto(0,150)
+    t.pendown()
+    t.goto(1000,150)
+    t.goto(-1000,150)
+    t.penup()
+
+    t.goto(0,200)
+    t.pendown()
+    t.goto(1000,200)
+    t.goto(-1000,200)
+    t.penup()
+
+    t.goto(0,250)
+    t.pendown()
+    t.goto(1000,250)
+    t.goto(-1000,250)
+    t.penup()
+
+
+
+    t.goto(0,300)
+    t.pendown()
+    t.goto(1000,300)
+    t.goto(-1000,300)
+    t.penup()
+
+    t.goto(0,350)
+    t.pendown()
+    t.goto(1000,350)
+    t.goto(-1000,350)
+    t.penup()
+
+    t.goto(0,400)
+    t.pendown()
+    t.goto(1000,400)
+    t.goto(-1000,400)
+    t.penup()
+
+    t.goto(0,450)
+    t.pendown()
+    t.goto(1000,450)
+    t.goto(-1000,450)
+    t.penup()
+
+    t.goto(0,500)
+    t.pendown()
+    t.goto(1000,500)
+    t.goto(-1000,500)
+    t.penup()
+
+    t.goto(0,550)
+    t.pendown()
+    t.goto(1000,550)
+    t.goto(-1000,550)
+    t.penup()
+
+    t.goto(0,600)
+    t.pendown()
+    t.goto(1000,600)
+    t.goto(-1000,600)
+    t.penup()
+
+
+    t.goto(0,-50)
+    t.pendown()
+    t.goto(1000,-50)
+    t.goto(-1000,-50)
+    t.penup()
+
+    t.goto(0,-100)
+    t.pendown()
+    t.goto(1000,-100)
+    t.goto(-1000,-100)
+    t.penup()
+
+    t.goto(0,-150)
+    t.pendown()
+    t.goto(1000,-150)
+    t.goto(-1000,-150)
+    t.penup()
+
+    t.goto(0,-200)
+    t.pendown()
+    t.goto(1000,-200)
+    t.goto(-1000,-200)
+    t.penup()
+
+    t.goto(0,-250)
+    t.pendown()
+    t.goto(1000,-250)
+    t.goto(-1000,-250)
+    t.penup()
+
+
+
+    t.goto(0,-300)
+    t.pendown()
+    t.goto(1000,-300)
+    t.goto(-1000,-300)
+    t.penup()
+
+    t.goto(0,-350)
+    t.pendown()
+    t.goto(1000,-350)
+    t.goto(-1000,-350)
+    t.penup()
+
+    t.goto(0,-400)
+    t.pendown()
+    t.goto(1000,-400)
+    t.goto(-1000,-400)
+    t.penup()
+
+    t.goto(0,-450)
+    t.pendown()
+    t.goto(1000,-450)
+    t.goto(-1000,-450)
+    t.penup()
+
+    t.goto(0,-500)
+    t.pendown()
+    t.goto(1000,-500)
+    t.goto(-1000,-500)
+    t.penup()
+
+    t.goto(0,-550)
+    t.pendown()
+    t.goto(1000,-550)
+    t.goto(-1000,-550)
+    t.penup()
+
+    t.goto(0,-600)
+    t.pendown()
+    t.goto(1000,-600)
+    t.goto(-1000,-600)
+    t.penup()
+
+  
+
+    t.goto(-750,600)
+    t.pendown()
+    t.goto(-750,-600)
+    t.penup()   
+
+    t.goto(-700,600)
+    t.pendown()
+    t.goto(-700,-600)
+    t.penup()   
+
+    t.goto(-650,600)
+    t.pendown()
+    t.goto(-650,-600)
+    t.penup() 
+
+    t.goto(-500,600)
+    t.pendown()
+    t.goto(-500,-600)
+    t.penup() 
+
+    t.goto(-450,600)
+    t.pendown()
+    t.goto(-450,-600)
+    t.penup() 
+
+    t.goto(-400,600)
+    t.pendown()
+    t.goto(-400,-600)
+    t.penup() 
+
+    t.goto(-350,600)
+    t.pendown()
+    t.goto(-350,-600)
+    t.penup() 
+
+    t.goto(-300,600)
+    t.pendown()
+    t.goto(-300,-600)
+    t.penup() 
+
+    t.goto(-250,600)
+    t.pendown()
+    t.goto(-250,-600)
+    t.penup() 
+
+    t.goto(-200,600)
+    t.pendown()
+    t.goto(-200,-600)
+    t.penup() 
+
+    t.goto(-100,600)
+    t.pendown()
+    t.goto(-100,-600)
+    t.penup() 
+
+    t.goto(-50,600)
+    t.pendown()
+    t.goto(-50,-600)
+    t.penup() 
+
+
+    t.goto(750,600)
+    t.pendown()
+    t.goto(750,-600)
+    t.penup()   
+
+    t.goto(700,600)
+    t.pendown()
+    t.goto(700,-600)
+    t.penup()   
+
+    t.goto(650,600)
+    t.pendown()
+    t.goto(650,-600)
+    t.penup() 
+
+    t.goto(600,600)
+    t.pendown()
+    t.goto(600,-600)
+    t.penup() 
+
+    t.goto(-600,600)
+    t.pendown()
+    t.goto(-600,-600)
+    t.penup()
+
+    t.goto(500,600)
+    t.pendown()
+    t.goto(500,-600)
+    t.penup() 
+
+    t.goto(450,600)
+    t.pendown()
+    t.goto(450,-600)
+    t.penup() 
+
+    t.goto(550,600)
+    t.pendown()
+    t.goto(550,-600)
+    t.penup() 
+
+    t.goto(-550,600)
+    t.pendown()
+    t.goto(-550,-600)
+    t.penup() 
+
+    t.goto(400,600)
+    t.pendown()
+    t.goto(400,-600)
+    t.penup() 
+
+    t.goto(350,600)
+    t.pendown()
+    t.goto(350,-600)
+    t.penup() 
+
+    t.goto(300,600)
+    t.pendown()
+    t.goto(300,-600)
+    t.penup() 
+
+    t.goto(250,600)
+    t.pendown()
+    t.goto(250,-600)
+    t.penup() 
+
+    t.goto(200,600)
+    t.pendown()
+    t.goto(200,-600)
+    t.penup() 
+
+    t.goto(150,600)
+    t.pendown()
+    t.goto(150,-600)
+    t.penup() 
+
+    t.goto(-150,600)
+    t.pendown()
+    t.goto(-150,-600)
+    t.penup()
+
+    t.goto(100,600)
+    t.pendown()
+    t.goto(100,-600)
+    t.penup() 
+
+    t.goto(50,600)
+    t.pendown()
+    t.goto(50,-600)
+    t.penup() 
+
+
+
+    a = float(input("a"))
+    b = float(input("b"))
+    c = float(input("c"))
+
+    x = -5
+        
+    x2 = x**2
+    an = x2*a
+    bn = b*x
+    y = an+bn+c
+    print(x2,an,bn,c,y)
+
+    q = 100*x
+    w = 100*y
+    t.goto(q,w)
+
+    for i in range(-300,300,1):
+            
+            
+
+            t.pencolor("red")
+            x = i/10
+        
+            x2 = x**2
+            an = x2*a
+            bn = b*x
+            y = an+bn+c
+            print(x2,an,bn,c,y)
+
+            q = 50*x
+            w = 50*y
+            t.goto(q,w)
+            t.pendown()
+            
+
+    input()
+    t.penup()
+    t.goto(1000,0)
